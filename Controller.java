21ccb04e17f106b6fcc5bea3673579f621c96422
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2024-03-29 13:31:26 +0300

    Добавила класс Контроллер

---
+++ Controller.java
@@ -0,0 +1,6 @@
+public class Controller {
+    private EducationalGroupService educationalGroupService;
+    public Controller() {
+        this.educationalGroupService = new EducationalGroupService();
+    }
+}
